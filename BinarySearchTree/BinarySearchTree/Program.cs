d455f06cd64d84f178b9287e4cd11ba054ce2648
Holger <horol18@student.sdu.dk>
2019-03-22 13:15:09 +0100

    Fucking mega done
    Not even close

--- BinarySearchTree/BinarySearchTree/Program.cs
+++ BinarySearchTree/BinarySearchTree/Program.cs
@@ -10,7 +10,44 @@ namespace BinarySearchTree
     {
         static void Main(string[] args)
         {
+            List<TreeNode> nodes;
 
+            Treevalues treevalues = new Treevalues();
+
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Enter Root Number:");
+
+            nodes = new List<TreeNode>();
+
+            int value = Convert.ToInt32(Console.ReadLine());
+            Tree tree = new Tree(value);
+
+            while (true)
+            {
+                
+                Console.WriteLine("Enter Input, Remove or Exit");
+
+                string input = Console.ReadLine();
+
+                switch (input)
+                {
+                    case "Input":
+                        Console.WriteLine("Enter New Node:");
+
+                        int node = Convert.ToInt32(Console.ReadLine());
+                        nodes.Add(new TreeNode(node));
+
+                        break;
+                    case "Remove":
+                        int index = Convert.ToInt32(Console.ReadLine());
+                        
+                        break;
+                    case "Exit":
+                        Environment.Exit(0);
+                        break;
+                }
+            }
         }
     }
 }
